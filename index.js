2359d6a0ac16ca2322987a3ffa31ea23a9671e96
Fahmudul Hassan <fahmudul7890@email.com>
2024-06-01 21:15:46 +0600

    created jwt token and sending to client

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 //TOdo : Payment API
 
 const port = process.env.PORT || 5000;
-
+app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -23,6 +23,11 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     const roomCollection = client
@@ -38,6 +43,18 @@ async function run() {
       const rooms = await roomCollection.find().toArray();
       res.send(rooms);
     });
+
+    // JWT Token API create jwt token
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      console.log("email from jwt", email);
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10d",
+      });
+      console.log(token);
+      res.send(token);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
