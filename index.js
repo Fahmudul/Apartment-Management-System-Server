f0f75cab4992a5c52fa79ed923b3ed90111c9b21
Fahmudul Hassan <fahmudul7890@email.com>
2024-06-01 09:40:09 +0600

    added room data and made routes for all room

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cors = require("cors");
+var jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+//TOdo : Payment API
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkzn5jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const roomCollection = client
+      .db("CozyNest")
+      .collection("AppartmentCollection");
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    // All Room API
+    app.get("/allRooms", async (req, res) => {
+      const rooms = await roomCollection.find().toArray();
+      res.send(rooms);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
-  res.send("Hello World!");
+  res.send("CozyNest Server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`CozyNest sitting on ${port}`);
 });
